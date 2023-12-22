ba19b9211dd8a684157760e3865390299315a6e2
bielt1 <bielt1@bfh.ch>
2023-12-22 14:28:28 +0100

    deploying 2.0

--- angular/src/app/main/blog/instagram.service.ts
+++ angular/src/app/main/blog/instagram.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 export class InstagramService {
   private apiUrl = 'https://graph.instagram.com/v12.0';
   private accessToken =
-    'IGQWRQa1RhV2dpcHhkSHFVa2xma3hZAalZALdjcwRGxpRTczZAkhkM21sYlpuclJEWGxvdDN1SS1oQVA5S1RHTzJZAOWFWVXpjQjhqU3N0eG8wbnFhRUdudWh0aVFGYjJIazRsS0tHZAEdTMUkwYzRjMzN2QUw1QnlEb3cZD';
+    'IGQWRNTDNhMzdRSGpaN3o5bV9obVJzelBhUUpJU3dCaTBCSndLY2hVMWRnWlRZAT2c4UktfZAUxuWlEtQldxM3U0WmpJbHRaUG01a3U2N2VuWUswT0toYVdRUU92d1YtaUtyZA0N4V2Y0NE5ucmJqMFlzaURBa21pZAjgZD';
 
   constructor(private http: HttpClient) {}
 
